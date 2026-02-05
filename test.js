62d1374dcd105c43a1e0c1c4504eab04142f8491
amine sakhri <sakhriaminee2003@gmail.com>
2026-02-05 16:06:43 +0100

    function supprimer

--- test.js
+++ test.js
@@ -1,15 +1,17 @@
 // console.log("test");
 
-const table =[
+let table =[
   {
-    titre: "Vacances à Paris",
+    id :1 ,
+    titre: "xxxxxxxxxxxx",
     destination: "Paris, France",
     date: "2024-07-15",
     note: 5,
-    categorie: "Beach",
+    categorie: "Forest",
     image : "https://th.bing.com/th/id/OIP.2LzIzZDD8L9Ff8FQbZD1NwHaE8?w=282&h=188&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3"
   },
   {
+     id :2 ,
     titre: "Vacances à Paris",
     destination: "Paris, France",
     date: "2024-07-15",
@@ -18,6 +20,7 @@ const table =[
     image : "https://th.bing.com/th/id/OIP.2LzIzZDD8L9Ff8FQbZD1NwHaE8?w=282&h=188&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3"
   },
    {
+     id :3,
     titre: "Vacances à Paris",
     destination: "Paris, France",
     date: "2024-07-15",
@@ -26,6 +29,7 @@ const table =[
     image : "https://th.bing.com/th/id/OIP.2LzIzZDD8L9Ff8FQbZD1NwHaE8?w=282&h=188&c=7&r=0&o=7&dpr=1.5&pid=1.7&rm=3"
   },
      {
+    id :4 ,
     titre: "Vacances à Paris",
     destination: "Paris, France",
     date: "2024-07-15",
@@ -49,6 +53,7 @@ bt.addEventListener("click", (e) => {
   e.preventDefault();
 
   let newtable = {
+    id : table.length+1,
     titre: titre.value,
     destination: destination.value,
     note: Number(note.value),
@@ -75,21 +80,25 @@ function CreeElement() {
     const x = table[i];
 
     container.innerHTML += `       
-      <div id="rr" class="w-[300px] rounded-[20px] shadow-lg  bg-white m-6">
+      <div " class="flex flex-wrap justify-center items-center  w-[350px] rounded-[20px] shadow-lg m-7  bg-white mx card">
         <img src="${x.image}" class="w-full h-[250px]  rounded-t-[20px]">
         <div class="p-4 text-center">
           <h2 class="text-sm font-bold">${x.titre}</h2>
           <p>${x.destination}</p>
           <p class="text-sm ">${x.note}</p>
           <p>${x.categorie}</p>
-         <button id="rmv"  class="bg-blue-600 w-[120px] py-3
-        rounded-[30px] text-white text-center"> supprimer </button>
+         <button  onclick= "supprimer(${x.id})"  class="bg-blue-600 w-[120px] py-3
+        rounded-[30px] text-white  text-center "> supprimer </button>
         </div>
       </div>
     `;
 }}CreeElement()
 
 
+function supprimer(id){
+  table = table.filter(g=>g.id !== id)
+ CreeElement(table);
+}
 
 
 
